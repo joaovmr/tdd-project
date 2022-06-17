f716f35c68285c7185dc8fa0b9cf6f41a33eeb74
joaovmr2000 <joao.morais@iebtdigital.com.br>
2022-06-17 16:48:33 -0300

    Refatorando o Teste Funcional

--- lists/tests.py
+++ lists/tests.py
@@ -10,3 +10,8 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
